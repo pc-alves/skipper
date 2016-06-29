321653348887e935e7323271afb9aabdb50c8506
Arpad Ryszka <arpad.ryszka@zalando.de>
2016-06-29 02:31:19 +0200

    add doc for httptesting package

--- skptesting/httptesting/http.go
+++ skptesting/httptesting/http.go
@@ -1,3 +1,7 @@
+/*
+Package httptesting provides testing utilities for tests
+running http servers.
+*/
 package httptesting
 
 import (
@@ -69,6 +73,7 @@ func (s servers) closePool() {
 	}
 }
 
+// Creates a new server pool.
 func NewServerPool() *ServerPool {
 	s := make(servers)
 	get := make(chan message)
@@ -93,22 +98,29 @@ func NewServerPool() *ServerPool {
 	return &ServerPool{get, release, quit}
 }
 
+// Takes a server from the pool. If there is no available idle server,
+// then it creates one. It sets the handler of the server to h.
 func (sp *ServerPool) Get(h http.Handler) *httptest.Server {
 	m := message{handler: h, response: make(chan *httptest.Server)}
 	sp.get <- m
 	return <-m.response
 }
 
+// Puts back an idle server into the pool.
 func (sp *ServerPool) Release(s *httptest.Server) {
 	m := message{server: s, response: make(chan *httptest.Server)}
 	sp.release <- m
 	<-m.response
 }
 
+// Closes the pool. It closes all servers that are currently
+// idle in the pool.
 func (sp *ServerPool) Close() {
 	close(sp.quit)
 }
 
+// Executes f with servers from the pool. It will take a server for
+// each handler passed in with h.
 func WithServers(h []http.Handler, f func([]*httptest.Server)) {
 	s := make([]*httptest.Server, len(h))
 	for i, hi := range h {
@@ -119,6 +131,7 @@ func WithServers(h []http.Handler, f func([]*httptest.Server)) {
 	f(s)
 }
 
+// Executes f with a server from the pool, using h.
 func WithServer(h http.Handler, f func(*httptest.Server)) {
 	WithServers([]http.Handler{h}, func(s []*httptest.Server) {
 		f(s[0])
